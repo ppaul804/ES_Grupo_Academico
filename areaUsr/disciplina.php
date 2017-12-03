f21a83f3a6570a041cfa455126881136cead7b3b
Pedro Paul <ppaul804@gmail.com>
2017-12-03 15:42:41 -0200

    Centralização dos botões na página Disciplina

--- areaUsr/disciplina.php
+++ areaUsr/disciplina.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="pt-br">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,6 +15,7 @@
         <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="shortcut icon" href="../img/favicon.png">
 
         <!-- Custom styles for this template -->
         <link href="../css/landing-page.css" rel="stylesheet">
@@ -27,10 +28,12 @@
         ?>
 
         <!-- Page Content -->
+        
+        <!--Criar Nova Disciplina-->
         <section class="content-section-b" style="margin-top: 25px">
             <div class="container">
                 <p class="h3 text-center"><strong>Disciplinas</strong></p>
-				
+
                 <form action="banco/disciplinaCadastrar.php" method="POST">
                     <div class="form-group">
                         <label for="inputDisciplina">Criar Nova Disciplina</label>
@@ -42,38 +45,42 @@
             </div>
         </section>
 
+        <!--Selecionar Disciplina-->
         <section class="content-section-a">
             <div class="container">
-			
-				<?php require "../banco/configuracao.php";
-				
-				session_start();
-				
-				$grupos = $_SESSION["grupos"];
-				$query = mysqli_query($conexao, "SELECT * FROM tb_disciplina WHERE fk_idt_grupo = $grupos"); ?>
-			
-				<form action="" method="POST"> 
-					<label>Selecionar Disciplina:</label> 
-					<select id="disciplinas" name="disciplinas"> 
-						<?php while($prod = mysqli_fetch_array($query)) { ?> 
-							<option value="<?php echo $prod['idt_disciplina'] ?>"><?php echo $prod['nme_disciplina'] ?></option> <?php
-                                $idDisciplina = $_POST["disciplinas"];																							
-						} ?> 
-					</select> 
-					<br>
-					<button class="btn btn-primary" type="submit">Selecionar</button>									
-				</form>							
+
+                <?php
+                require "../banco/configuracao.php";
+
+                session_start();
+
+                $grupos = $_SESSION["grupos"];
+                $query = mysqli_query($conexao, "SELECT * FROM tb_disciplina WHERE fk_idt_grupo = $grupos");
+                ?>
+
+                <form action="" method="POST"> 
+                    <label>Selecionar Disciplina:</label> 
+                    <select id="disciplinas" name="disciplinas"> 
+                        <?php while ($prod = mysqli_fetch_array($query)) { ?> 
+                            <option value="<?php echo $prod['idt_disciplina'] ?>"><?php echo $prod['nme_disciplina'] ?></option> <?php
+                        $idDisciplina = $_POST["disciplinas"];
+                    }
+                        ?> 
+                    </select> 
+                    <br>
+                    <button class="btn btn-primary" type="submit">Selecionar</button>									
+                </form>							
             </div>
         </section>
-  
+
+        <!--Notações-->
         <section class="content-section-b">
             <div class="conteiner">
                 <p class="h4 text-center"><strong>Notações</strong></p>
-				                 
                 <br>
                 <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="col-md-4">
+                    <label class="col-md-4 control-label"></label>
+                    <div class="col-md-4 text-center" >
                         <table class="table table-hover table-striped">
                             <tr>
                             <strong>
@@ -99,109 +106,95 @@
                 <br>
             </div>
             <!--Botões-->
-            <div class="container" >
-                <div>							
-				
-                    <form action="inserirNota.php" method "POST">                       
-                         <input type="hidden" name="idDisciplina" value="<?php echo $idDisciplina; ?>" />  
-						   <button id="inserir" name="" class="btn btn-primary">Inserir</button> 							
+            <div class="row" >
+                <label class="col-md-4 control-label"></label>
+                <div class="col-md-4 text-center">
+                    <form action="inserirNota.php" method="POST">                       
+                        <input type="hidden" name="idDisciplina" value="<?php echo $idDisciplina; ?>" />  
+                        <button id="inserir" name="" class="btn btn-primary">Inserir</button> 							
                     </form>
-                <br>
+                    <br>
                     <form action="editaNota.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST"> 
-                            <button id="editar" name="editar" class="btn btn-primary">Editar</button>
-							<input id="inputCodigoEditar" name="inputCodigoEditar" type="text">
+                        <button id="editar" name="editar" class="btn btn-primary">Editar</button>
+                        <input id="inputCodigoEditar" name="inputCodigoEditar" type="text">
                     </form>
-                <br>
+                    <br>
                     <form action="banco/excluirNota.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST">  
-                            <button id="excluir" name="excluir" class="btn btn-primary">Excluir</button> 
-							  <input id="inputCodigo" name="inputCodigo" type="text">
-							  <br>
-							 
+                        <button id="excluir" name="excluir" class="btn btn-primary">Excluir</button> 
+                        <input id="inputCodigo" name="inputCodigo" type="text">
+                        <br>
                     </form>
-
                 </div>
             </div>
         </section>
-		 						
-                     
+
+        <!--Arquivos-->
         <section class="content-section-a">
             <div class="conteiner">
-			 
+
                 <p class="h4 text-center"><strong>Arquivos</strong></p>
                 <br>
                 <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="col-md-4">
-			  
-			
-				        <table class="table table-hover table-striped">
-                            <tr>
-					
+                <label class="col-md-4 control-label"></label>
+                    <div class="col-md-4 text-center">
+                        <table class="table table-hover table-striped">
+                            <tr>	
                             <strong>
                                 <th>Código</th>
                                 <th>Autor</th>
                                 <th>Descrição</th>
-								<th>Arquivo</th>
+                                <th>Arquivo</th>
                             </strong>
-        
                             </tr>
-							
-							<tr>							
+                            <tr>							
                                 <td> </td>
-								<td> </td>
-								<td> </td>
-								<td> </td>
-								
+                                <td> </td>
+                                <td> </td>
+                                <td> </td>
                             </tr>
-							
                             <tr>
                                 <td></td>
                                 <td></td>
                                 <td></td>
-								<td> </td>
-								
+                                <td></td>
                             </tr>
-							
-							 <tr>
+                            <tr>
                                 <td></td>
                                 <td></td>
                                 <td></td>
-								<td> </td>
-								
+                                <td></td>
                             </tr>
-							
-						</table>
-			
+                        </table>
                     </div>
                 </div>
             </div>
-		
+
             <!--Botões-->
-			
-            <div class="container">
+
+            <div class="row">
+                <label class="col-md-4 control-label"></label>
                 <div class="col-md-4 text-center"> 
-			
-				<form action="inserirArquivo.php" method "POST">
-				 <input type="hidden" name="idDisciplina" value="<?php echo $idDisciplina; ?>" />  
-                    <button id="inserir" name="" class="btn btn-primary">Inserir</button> 
-			    </form>
-		     <br>
-			 
-			 <form action="banco/excluirArquivo.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST">  
-                    <button id="excluir" name="" class="btn btn-primary">Excluir</button> 
-					   <input id="inputCodigoArquivo" name="inputCodigoArquivo" type="text">
-                </form>
-				</div>
+                    <form action="inserirArquivo.php" method="POST">
+                          <input type="hidden" name="idDisciplina" value="<?php echo $idDisciplina; ?>" />  
+                        <button id="inserir" name="" class="btn btn-primary">Inserir</button> 
+                    </form>
+                    <br>	 
+                    <form action="banco/excluirArquivo.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST">  
+                        <button id="excluir" name="" class="btn btn-primary">Excluir</button> 
+                        <input id="inputCodigoArquivo" name="inputCodigoArquivo" type="text">
+                    </form>
+                </div>
             </div>
         </section>
 
+        <!--Eventos-->
         <section class="content-section-b">
             <div class="conteiner">
                 <p class="h4 text-center"><strong>Eventos</strong></p>
                 <br>
                 <div class="row">
-                    <div class="col-md-4"></div>
-                    <div class="col-md-4">
+                <label class="col-md-4 control-label"></label>
+                    <div class="col-md-4 text-center">
                         <table class="table table-hover table-striped">
 
                             <tr>
@@ -229,21 +222,22 @@
                 </div>
             </div>
             <!--Botões-->
-            <div class="container">
-                <div> 
-              <form action="inserirEvento.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST">                    
-                 <button id="inserir" name="inserir" class="btn btn-primary">Inserir</button> 
-              </form>
-                <br>    
-             <form action="editaEvento.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST" =>                
-                  <button id="editar" name="editar" class="btn btn-primary">Editar</button> 
-				   <input id="inputCodigoEventoEditar" name="inputCodigoEventoEditar" type="text">
-             </form>
-                  <br>
-				  <form action="banco/excluirEvento.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST">  
-             <button id="excluir" name="excluir" class="btn btn-primary">Excluir</button>
-				 <input id="inputCodigoEvento" name="inputCodigoEvento" type="text">
-				   </form>
+            <div class="row">
+                <label class="col-md-4 control-label"></label>
+                <div class="col-md-4 text-center">
+                    <form action="inserirEvento.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST">                    
+                        <button id="inserir" name="inserir" class="btn btn-primary">Inserir</button> 
+                    </form>
+                    <br>    
+                    <form action="editaEvento.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST">                
+                        <button id="editar" name="editar" class="btn btn-primary">Editar</button> 
+                        <input id="inputCodigoEventoEditar" name="inputCodigoEventoEditar" type="text">
+                    </form>
+                    <br>
+                    <form action="banco/excluirEvento.php?idDisciplina=<?php echo $idDisciplina; ?>" method="POST">  
+                        <button id="excluir" name="excluir" class="btn btn-primary">Excluir</button>
+                        <input id="inputCodigoEvento" name="inputCodigoEvento" type="text">
+                    </form>
                 </div>
             </div>
         </section>
